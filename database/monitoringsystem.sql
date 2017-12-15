7cec20e6d454478aeddbfec6bade60fb114c87f9
rolex <rolexjun098@gmail.com>
2017-12-16 06:59:12 +0800

    updated monitoringsystem.sql

--- database/monitoringsystem.sql
+++ database/monitoringsystem.sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2017 at 10:48 PM
+-- Generation Time: Dec 15, 2017 at 11:57 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -19,9 +19,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
-create database `monitoringsystem`;
-use monitoringsystem;
+-- Database: `monitoringsystem`
 --
+
+DELIMITER $$
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `total_revenue_all_project` () RETURNS FLOAT BEGIN
+   DECLARE total FLOAT DEFAULT 0.0;
+   SELECT  
+		SUM(ss.amount)  INTO total
+        FROM products p 
+		INNER JOIN sales_specific ss ON ss.product_id=p.product_id 
+		INNER JOIN sales_record sr ON ss.or_number=sr.sales_id 
+		WHERE YEAR(sr.sold_date)= YEAR(CURRENT_DATE()) ;
+
+RETURN total;
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -40,19 +57,10 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`customer_id`, `customer_name`, `customer_address`) VALUES
-(3, 'df', 'df'),
-(4, 's', 's'),
-(5, 'd', 'd'),
-(6, 'd', 'd'),
-(7, 'd', 'd'),
-(8, 'h', 'h'),
-(9, 'df', 'fg'),
-(10, 'df', 'fg'),
-(11, 'd', 'd'),
-(12, 'd', 'd'),
-(13, 'd', 'd'),
-(14, 'd', 'd'),
-(15, 'd', 'd');
+(1, 'Christine Gondaya', 'Silago'),
+(2, 'James Ogang', 'Hinunangan'),
+(3, 'Christine', 'Hinunangan'),
+(4, 'Jacob Tayom', 'Ambacon');
 
 -- --------------------------------------------------------
 
@@ -76,15 +84,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `product_price`, `created_on`, `created_by`, `project_id`, `unit_of_measurement`, `product_status`) VALUES
-(2, 'Coconut Seedling', 4, '2017-12-11 17:37:54', NULL, 1, 'seedling', 'Y'),
-(3, 'Vinegar', 5, '2017-12-11 17:39:09', NULL, 1, 'Gallon', 'N'),
-(4, 'Charcoal', 6, '2017-12-11 17:41:58', NULL, 1, 'Taro', 'N'),
-(5, 'Vermi Worm', 7, '2017-12-11 17:46:13', NULL, 2, 'Kilo', 'Y'),
-(6, 'Vermi Compost', 8, '2017-12-11 18:57:39', NULL, 2, 'Kilo', 'Y'),
-(7, 'sample', 9, '2017-12-11 18:58:59', NULL, 2, 'kilo', 'Y'),
-(8, 'Bamboo Chair', 10, '2017-12-11 21:20:06', NULL, 4, 'strands', 'N'),
-(13, 'Sample', 15, '2017-12-11 22:05:26', NULL, 2, 'kilo', 'Y'),
-(14, 'sample1', 16, '2017-12-11 22:49:47', NULL, 1, 'seedling', 'Y');
+(1, 'Coconut Seedling', 1, '2017-12-15 14:18:40', NULL, 1, 'Seedling', 'Y'),
+(2, 'Vinegar/Suka', 2, '2017-12-15 14:19:30', NULL, 1, 'Gallon', 'Y');
 
 -- --------------------------------------------------------
 
@@ -104,18 +105,8 @@ CREATE TABLE `product_price` (
 --
 
 INSERT INTO `product_price` (`price_id`, `price`, `created_updated_on`, `created_by`) VALUES
-(1, 10, '2017-12-11 17:36:07', 0),
-(2, 10, '2017-12-11 17:36:14', 0),
-(3, 10, '2017-12-11 17:37:03', 0),
-(4, 10, '2017-12-11 17:37:54', 0),
-(5, 100, '2017-12-11 17:39:09', 0),
-(6, 20, '2017-12-11 17:41:58', 0),
-(7, 100, '2017-12-11 17:46:13', 0),
-(8, 1000, '2017-12-11 18:57:39', 0),
-(9, 20, '2017-12-11 18:58:59', 0),
-(10, 10, '2017-12-11 21:20:06', 0),
-(15, 1000, '2017-12-11 22:05:26', 0),
-(16, 10, '2017-12-11 22:49:47', 0);
+(1, 100, '2017-12-15 14:18:40', 0),
+(2, 100, '2017-12-15 14:19:30', 0);
 
 -- --------------------------------------------------------
 
@@ -140,10 +131,8 @@ CREATE TABLE `projects` (
 --
 
 INSERT INTO `projects` (`project_id`, `project_name`, `project_description`, `project_status`, `project_incharge`, `project_started`, `project_ended`, `created_on`, `created_by`) VALUES
-(1, 'Coconut', '                                             ', 'N', 1, '2017-12-11', '0000-00-00', '2017-12-11 17:35:30', NULL),
-(2, 'Vermi', '', 'Y', 1, '2017-12-12', NULL, '2017-12-11 17:45:42', NULL),
-(3, 'Goats', '                                             ', 'Y', 1, '2017-12-12', NULL, '2017-12-11 21:13:24', NULL),
-(4, 'Bamboos', '                                             ', 'N', 1, '2017-12-11', '0000-00-00', '2017-12-11 21:17:51', NULL);
+(1, 'Coconut', 'This is a project of DA Maasin               ', 'Y', 1, '2017-12-13', '0000-00-00', '2017-12-15 14:17:37', NULL),
+(2, 'Vinegar', 'This is a project of DA                      ', 'Y', 1, '2017-12-13', '0000-00-00', '2017-12-15 14:18:20', NULL);
 
 -- --------------------------------------------------------
 
@@ -166,19 +155,10 @@ CREATE TABLE `sales_record` (
 --
 
 INSERT INTO `sales_record` (`sales_id`, `or_number`, `total_amount`, `mode_of_payment`, `sold_date`, `user_id`, `customer_id`) VALUES
-(5, 'ddddd', 1000, 'cash', '2017-12-12 20:51:33', 1, 3),
-(6, '11111', 1000, 'cash', '2017-12-12 20:53:26', 1, 4),
-(7, 'sfsf', 1000, 'cash', '2017-12-12 22:34:29', 1, 5),
-(8, '222222', 1000, 'cash', '2017-12-12 22:35:26', 1, 6),
-(9, 'dfdfd', 10, 'cash', '2017-12-12 22:50:19', 1, 7),
-(10, 'fgfgfg', 40, 'cash', '2017-12-12 22:51:03', 1, 8),
-(11, 'dfsf', 0, 'cash', '2017-12-13 14:58:24', 1, 9),
-(12, 'dfsf', 0, 'cash', '2017-12-13 14:58:33', 1, 10),
-(13, 'd', 0, 'cash', '2017-12-13 15:00:58', 1, 11),
-(14, '1', 0, 'cash', '2017-12-13 15:01:53', 1, 12),
-(15, '1', 0, 'cash', '2017-12-13 15:01:58', 1, 13),
-(16, 'd', 1080, 'cash', '2017-12-13 16:03:08', 1, 14),
-(17, 'd', 1010, 'cash', '2017-12-13 16:06:29', 1, 15);
+(1, '1121211', 300, 'cash', '2017-12-15 14:20:28', 1, 1),
+(2, '121132232', 100, 'cash', '2017-12-15 14:24:21', 1, 2),
+(3, '345454333', 400, 'cash', '2017-11-15 14:27:00', 1, 3),
+(4, '11121121', 900, 'cash', '2017-12-15 16:51:16', 1, 4);
 
 -- --------------------------------------------------------
 
@@ -199,27 +179,12 @@ CREATE TABLE `sales_specific` (
 --
 
 INSERT INTO `sales_specific` (`sales_specific_id`, `product_id`, `quantity`, `amount`, `or_number`) VALUES
-(5, 2, 2, 20, 5),
-(6, 14, 1, 10, 5),
-(7, 5, 1, 100, 6),
-(8, 14, 1, 10, 6),
-(9, 2, 1, 10, 6),
-(10, 14, 1, 10, 7),
-(11, 2, 1, 10, 7),
-(12, 6, 1, 1, 8),
-(13, 14, 1, 10, 8),
-(14, 2, 2, 20, 8),
-(15, 2, 1, 10, 9),
-(16, 2, 4, 40, 10),
-(17, 2, 2, 20, 11),
-(18, 2, 2, 20, 12),
-(19, 2, 1, 10, 13),
-(20, 2, 1, 10, 14),
-(21, 2, 1, 10, 15),
-(22, 7, 4, 80, 16),
-(23, 13, 1, 1, 16),
-(24, 13, 1, 1, 17),
-(25, 2, 1, 10, 17);
+(1, 2, 1, 100, 1),
+(2, 1, 2, 200, 1),
+(3, 1, 1, 100, 2),
+(4, 2, 1, 100, 3),
+(5, 1, 3, 300, 3),
+(6, 2, 9, 900, 4);
 
 -- --------------------------------------------------------
 
@@ -245,8 +210,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `username`, `password`, `hint`, `status`, `profile_pic`, `user_type`, `created_on`) VALUES
-(1, 'Rolly', 'Tereso', 'admin', 'ƒ¡‡X¼“ÇÃ²üÀ‰£', '111', 'N', 'img/Christmas-Hat-PNG-HD.png', 1, '2017-12-11 17:35:10'),
-(2, 'Christine', 'Gondaya', 'christine', 'skÜ…°Ï’ôÇä¥kO\0', 'Case Number', 'N', NULL, 1, '2017-12-12 17:24:05');
+(1, 'Rolly', 'Tereso', 'rolex', 'jã#ïÔ®í5NB¬¹Àp—', 'My Case Number', 'N', NULL, 1, '2017-12-15 14:15:39');
 
 --
 -- Indexes for dumped tables
@@ -285,8 +249,8 @@ ALTER TABLE `projects`
 --
 ALTER TABLE `sales_record`
   ADD PRIMARY KEY (`sales_id`),
-  ADD KEY `fuserId_idx` (`user_id`),
-  ADD KEY `fk_customer_idx` (`customer_id`);
+  ADD KEY `fk_customer_idx` (`customer_id`),
+  ADD KEY `fuserId_idx` (`user_id`);
 
 --
 -- Indexes for table `sales_specific`
@@ -310,37 +274,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `product_price`
 --
 ALTER TABLE `product_price`
-  MODIFY `price_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `price_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `sales_record`
 --
 ALTER TABLE `sales_record`
-  MODIFY `sales_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `sales_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `sales_specific`
 --
 ALTER TABLE `sales_specific`
-  MODIFY `sales_specific_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `sales_specific_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
